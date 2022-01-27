9a40da5a90c408a966a8f524a3af4e9fd1cc85dc
zhangmingrui <mingrui.zhang@cyberserval.cn>
2022-01-27 18:18:37 +0800

    feat: popconfirm组件

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,6 @@ export { default as Drawer } from './drawer';
 
 export type { ModalProps, ModalFuncProps } from './modal';
 export { default as Modal } from './modal';
+
+export type { PopconfirmProps } from './popconfirm';
+export { default as Popconfirm } from './popconfirm';
---
+++ src/popconfirm/Popconfirm.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { Popconfirm as APopconfirm } from 'antd';
+import type { PopconfirmProps as APopconfirmProps } from 'antd';
+
+import './style/index.less';
+
+interface PopconfirmProps extends APopconfirmProps {
+  /** 标题名字 */
+  titleName?: string;
+}
+
+export const Popconfirm: React.FC<PopconfirmProps> = (props) => {
+  const { titleName, icon = null, title, children, ...rest } = props;
+  let titleR = title;
+  if (titleName) {
+    titleR = (
+      <>
+        <div className="svl-popconfirm-title">{titleName}</div>
+        {title}
+      </>
+    );
+  }
+  return (
+    <APopconfirm icon={icon} title={titleR} {...rest}>
+      {children}
+    </APopconfirm>
+  );
+};
+
+export default Popconfirm;
+
+export type { PopconfirmProps };
