b27c5a4823386a308799c30b52d6508d68df814c
xueyang996 <xueyang996@gmail.com>
2022-08-15 10:54:15 +0800

    fix: Select未配置ref属性

--- src/tree-select/index.tsx
+++ src/tree-select/index.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import { TreeSelect as ATreeSelect } from 'antd';
 import type { TreeSelectProps } from 'antd';
+import { BaseOptionType, DefaultOptionType } from 'antd/lib/select';
+import type { BaseSelectRef } from 'rc-select';
 import { PullDown } from '../const';
 
 const { TreeNode, SHOW_ALL, SHOW_CHILD, SHOW_PARENT } = ATreeSelect;
@@ -15,15 +17,19 @@ const TreeSelectR: React.FC<TreeSelectProps> = (props) => {
 
 export { TreeNode, SHOW_ALL, SHOW_PARENT, SHOW_CHILD };
 
-type InternalTreeSelectType = typeof TreeSelectR;
-interface TreeSelectInterface extends InternalTreeSelectType {
+const TreeSelect = TreeSelectR as (<
+  ValueType = any,
+  OptionType extends BaseOptionType | DefaultOptionType = DefaultOptionType,
+>(
+  props: React.PropsWithChildren<TreeSelectProps<ValueType, OptionType>> & {
+    ref?: React.Ref<BaseSelectRef>;
+  },
+) => React.ReactElement) & {
   TreeNode: typeof TreeNode;
   SHOW_ALL: typeof SHOW_ALL;
   SHOW_PARENT: typeof SHOW_PARENT;
   SHOW_CHILD: typeof SHOW_CHILD;
-}
-
-const TreeSelect = TreeSelectR as TreeSelectInterface;
+};
 
 TreeSelect.TreeNode = TreeNode;
 TreeSelect.SHOW_ALL = SHOW_ALL;
